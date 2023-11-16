0a5ef314575db4d5358deaccd7ae980c1dc951c1
voiddragon234 <128116071+voiddragon234@users.noreply.github.com>
2023-11-17 09:12:52 +1100

    Update maps.py

--- files_and_assets/maps.py
+++ files_and_assets/maps.py
@@ -1,8 +1,1 @@
-maps = ["http://www.delugerpg.com/map/1", "http://www.delugerpg.com/map/2", "http://www.delugerpg.com/map/3", 
-        "http://www.delugerpg.com/map/4", "http://www.delugerpg.com/map/5", "http://www.delugerpg.com/map/6", 
-        "http://www.delugerpg.com/map/8",
-        "http://www.delugerpg.com/map/9", "http://www.delugerpg.com/map/10", "http://www.delugerpg.com/map/11", 
-        "http://www.delugerpg.com/map/12", "http://www.delugerpg.com/map/14", "http://www.delugerpg.com/map/snowcave1",
-        "http://www.delugerpg.com/map/volcano2", "http://www.delugerpg.com/map/powerplant2", "https://www.delugerpg.com/map/pkmnmansion1",
-        "https://www.delugerpg.com/map/pkmntower2", "https://www.delugerpg.com/map/pkmntower3"
-    ]
+maps = ["https://www.delugerpg.com/map/rockcave2"]
